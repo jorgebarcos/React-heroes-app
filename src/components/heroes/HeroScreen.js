3275a4e303f9d35f178d028b2ea912b4fa91c334
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-18 09:32:37 -0300

    Estilo del componente HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams, Redirect } from 'react-router-dom'
 import { getHeroesById } from '../../selectors/getHeroesById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
 
      const {heroeId} = useParams();
 
@@ -12,19 +12,49 @@ export const HeroScreen = () => {
          return <Redirect to="/" />
       }
 
+      const handleReturn = () => {
+            
+        if ( history.length <= 2 ) {
+            history.push('/');
+        } else {
+            history.goBack();
+        }
+      }
+
       const {
           superhero,
           publisher,
           alter_ego,
-          fist_appearance,
+          first_appearance,
           characters,
       } = hero;
 
 
 
     return (
-        <div>
-            <h1>HeroScreen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img src={`../assets/${heroeId}.jpg`} alt={superhero}
+                className="img-thumbnail"
+                
+                />
+            </div>
+
+            <div className="col-8">
+                <h3>{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b> Alter ego: </b> { alter_ego } </li>
+                    <li className="list-group-item"><b> Publisher: </b> {publisher} </li>
+                    <li className="list-group-item"><b> First appearance: </b>{ first_appearance }</li>
+                </ul>
+                <h5> Characters </h5>
+                    <p> {characters}  </p>
+
+                    <button 
+                        className="btn btn-outline-info"
+                        onClick={handleReturn}
+                    >Return</button>
+            </div>
         </div>
     )
 }
