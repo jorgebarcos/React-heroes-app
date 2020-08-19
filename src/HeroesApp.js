ba3d23900008ea73d762107ac173bbe8ba7ea1f4
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-19 09:09:55 -0300

    Login de un usuario

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import { AppRouter } from './routers/AppRouter'
 import { AuthContext } from './auth/AuthContext'
 import { authReducer } from './auth/authReducer'
@@ -11,6 +11,10 @@ export const HeroesApp = () => {
 
     const [user, dispatch] = useReducer(authReducer, {}, init)
 
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user))
+        
+    }, [user])
 
     return (
         <AuthContext.Provider value={{ user, dispatch }}>
